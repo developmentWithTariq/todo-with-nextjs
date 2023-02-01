bdcab7f31e5dffcbcca764d7442623da68ee4fcc
Tariq Nawaz <tariqnawaz15994@gmail.com>
2023-02-01 10:05:38 +0500

    Update todoList.js

--- app/todoList.js
+++ app/todoList.js
@@ -1,25 +1,53 @@
-import React from 'react'
-import Todo from './todo'
+import Todo from "./todo";
 
 const getTodos = async () => {
-    let todos = await fetch('http://localhost:3000/api/todo/list');
-    return todos.json()
-}
+  let todos = await fetch("http://localhost:3001/api/todo/list");
+  return todos.json();
+};
 
-const TodoList = async () => {
-    const {todos}  = await getTodos()
+export default async function TodoList() {
+  const { todos } = await getTodos();
+
   return (
     <div>
-        <ul style={{ listStyleType: "none" , padding: 0}}>
-            {todos.map((t)=> (
-                 <li key={t.id} style={{padding: "5px 0"}}>                 
-                    <Todo todo = {t} />
-                </li>         
-            )                          
-            )}
-        </ul>
-   </div>
-  )
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        {todos.map((t) => {
+          return (
+            <li key={t.id} style={{ padding: "5px 0" }}>
+              <Todo todo={t} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default TodoList
+
+// import React from 'react'
+// import Todo from './todo'
+
+// const getTodos = async () => {
+//     let todos = await fetch('http://localhost:3000/api/todo/list');
+//     return todos.json()
+// }
+
+// const TodoList = async () => {
+//     const {todos}  = await getTodos()
+//   return (
+//     <div>
+//         <ul style={{ listStyleType: "none" , padding: 0}}>
+//             <>{todos.map((t)=> {
+//                 return (
+//                  <li key={t.id} style={{padding: "5px 0"}}>                 
+//                     <Todo todo = {t} />
+//                 </li>         
+//             )}                          
+//             )}
+//             </>
+//         </ul>
+//    </div>
+//   )
+// }
+
+// export default TodoList
