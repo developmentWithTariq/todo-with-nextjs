299404eac69ad4decefda4d13599c0594656e0b5
Tariq Nawaz <tariqnawaz15994@gmail.com>
2023-02-01 10:22:06 +0500

    Update todoList.js

--- app/todoList.js
+++ app/todoList.js
@@ -1,53 +1,27 @@
-import Todo from "./todo";
+import React from 'react'
+import Todo from './todo'
 
 const getTodos = async () => {
-  let todos = await fetch("http://localhost:3001/api/todo/list");
-  return todos.json();
-};
-
-export default async function TodoList() {
-  const { todos } = await getTodos();
-
-  return (
-    <div>
-      <ul style={{ listStyleType: "none", padding: 0 }}>
-        {todos.map((t) => {
-          return (
-            <li key={t.id} style={{ padding: "5px 0" }}>
-              <Todo todo={t} />
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+    let todos = await fetch('http://localhost:3000/api/todo/list');
+    return todos.json()
 }
 
+const TodoList = async () => {
+    const {todos}  = await getTodos()
+  return (
+    <div>
+        <ul style={{ listStyleType: "none" , padding: 0}}>
+            <>{todos.map((t)=> {
+                return (
+                 <li key={t.id} style={{padding: "5px 0"}}>                 
+                    <Todo todo = {t} />
+                </li>         
+            )}                          
+            )}
+            </>
+        </ul>
+   </div>
+  )
+}
 
-// import React from 'react'
-// import Todo from './todo'
-
-// const getTodos = async () => {
-//     let todos = await fetch('http://localhost:3000/api/todo/list');
-//     return todos.json()
-// }
-
-// const TodoList = async () => {
-//     const {todos}  = await getTodos()
-//   return (
-//     <div>
-//         <ul style={{ listStyleType: "none" , padding: 0}}>
-//             <>{todos.map((t)=> {
-//                 return (
-//                  <li key={t.id} style={{padding: "5px 0"}}>                 
-//                     <Todo todo = {t} />
-//                 </li>         
-//             )}                          
-//             )}
-//             </>
-//         </ul>
-//    </div>
-//   )
-// }
-
-// export default TodoList
+export default TodoList
